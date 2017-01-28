18e91a47950e04beff0f52bf348aa08ccf168981
Ben Costrell <bencostrell@gmail.com>
2017-01-27 19:19:59 -0500

    player movement, other circle there chillin

---
+++ Assets/Scripts/CircleController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleController : MonoBehaviour {
+
+	public Color circleColor;
+
+	// Use this for initialization
+	void Start () {
+		SetColor (circleColor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SetColor(Color colorToSet){
+		GetComponent<SpriteRenderer> ().color = colorToSet;
+		circleColor = colorToSet;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Reset")){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+
+		Move ();
+	}
+
+	void Move(){
+		Vector2 direction = Vector2.zero;
+		if (Input.GetAxis("Horizontal") > 0){
+			direction += Vector2.right;
+		}
+		if (Input.GetAxis ("Horizontal") < 0){
+			direction += Vector2.left;
+		}
+		if (Input.GetAxis("Vertical") > 0){
+			direction += Vector2.up;
+		}
+		if (Input.GetAxis ("Vertical") < 0){
+			direction += Vector2.down;
+		}
+		GetComponent<Rigidbody2D> ().velocity = speed * direction.normalized;
+	}
+}
